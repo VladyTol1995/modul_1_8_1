710472a32de0ba3dfc6494ace812dd5726e8aee8
VladyTol1995 <tolstosheevvladimir4@gmail.com>
2024-12-12 11:42:21 +0300

    Add files via upload

---
+++ modul_1_8_1.py
@@ -0,0 +1,15 @@
+my_dict = {"July":1997, "Vlad":1995, "Brony":2023}
+print(my_dict)
+print(my_dict.get("Vlad"))
+print(my_dict.get("John"))
+my_dict.update({"Sofi":2012, "Sam":1967})
+print(my_dict)
+my_dict.pop("July")
+print(my_dict)
+print(my_dict.get("July"))
+my_set = {25, "Apple", False, 766, 25, False, "Orange"}
+print(my_set)
+my_set.update({"Game Over", 1995})
+print(my_set)
+my_set.discard(False)
+print(my_set)
